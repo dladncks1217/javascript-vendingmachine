343ec493fe6c58efc7267e8eb65cef34d9ffaf0b
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-13 17:44:47 +0900

    feat(verify - drinkList): drinkList 입력값 검증 유틸함수 추가

--- src/utils/verify.js
+++ src/utils/verify.js
@@ -3,6 +3,11 @@ const verify = {
     if (Number.isNaN(Number(input))) throw new Error('[ERROR]');
     if (Number(input) % 10 !== 0) throw new Error('[ERROR]');
   },
+
+  drinkList(input) {
+    const data = input.split(';');
+    if (data[0] === input) throw new Error('[ERROR]');
+  },
 };
 
 module.exports = verify;
