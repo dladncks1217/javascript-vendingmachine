792a124d87f3fa1f88a69fdc165f17e228bcb802
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-14 14:33:29 +0900

    test(changesResult): 최종 잔돈 반환 결과 계산 유틸함수 테스트

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -1,4 +1,5 @@
 const { Random } = require('@woowacourse/mission-utils');
+const changesResult = require('../src/utils/changesResult');
 const initCoins = require('../src/utils/initCoins');
 
 describe('유틸함수 목록 테스트', () => {
@@ -9,5 +10,9 @@ describe('유틸함수 목록 테스트', () => {
     Random.pickNumberInRange.mockReturnValueOnce(2);
 
     expect(initCoins(270)).toEqual([0, 2, 1, 2]);
+  });
+
+  test('최종 잔돈 반환 결과 계산 유틸함수', () => {
+    expect(changesResult(660, [0, 4, 4, 10])).toEqual([0, 4, 4, 6]);
   });
 });
